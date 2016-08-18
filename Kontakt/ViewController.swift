19e0358fe4bc01dad4e034a83baf6964517882c7
Tim Whiting <timothy_whiting@hotmail.com>
2016-08-18 10:16:51 +0200

    Initial Commit

---
+++ Kontakt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kontakt
+//
+//  Created by Timothy Whiting on 18/08/2016.
+//  Copyright © 2016 Arcopo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
